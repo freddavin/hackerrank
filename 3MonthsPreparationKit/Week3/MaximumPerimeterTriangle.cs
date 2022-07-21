9798f4c780f6cb6655e5ad07b6a2a1267317b836
Fred Davin <fredericodavin@gmail.com>
2022-07-21 19:37:58 -0300

    Adicionando os desafios da semana 3

---
+++ 3MonthsPreparationKit/Week3/MaximumPerimeterTriangle.cs
@@ -0,0 +1,76 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'maximumPerimeterTriangle' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY sticks as parameter.
+     */
+
+    public static List<int> maximumPerimeterTriangle(List<int> sticks)
+    {
+        int[] maxSticksTriangle = new int[] { -1 };
+        long sumMaxSticksTriagle = -1;
+        for (int i = 0; i < sticks.Count; i++)
+        {
+            for (int j = i + 1; j < sticks.Count; j++)
+            {
+                for (int k = j + 1; k < sticks.Count; k++)
+                {
+                    long fisrtStick = sticks[i];
+                    long secondStick = sticks[j];
+                    long thirdStick = sticks[k];
+
+                    if (fisrtStick + secondStick > thirdStick &&
+                        fisrtStick + thirdStick > secondStick &&
+                        secondStick + thirdStick > fisrtStick)
+                    {
+                        long sumSticks = fisrtStick + secondStick + thirdStick;
+                        if (sumSticks > sumMaxSticksTriagle)
+                        {
+                            sumMaxSticksTriagle = sumSticks;
+                            maxSticksTriangle = new int[] { (int)fisrtStick, (int)secondStick, (int)thirdStick };
+                        }
+                    }
+
+                }
+            }
+        }
+        return maxSticksTriangle.ToList().OrderBy(n => n).ToList();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> sticks = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sticksTemp => Convert.ToInt32(sticksTemp)).ToList();
+
+        List<int> result = Result.maximumPerimeterTriangle(sticks);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ 3MonthsPreparationKit/Week3/SalesByMatch.cs
@@ -0,0 +1,66 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int totalOfSocks, List<int> pileOfSocks)
+    {
+        int[] countSockByColor = new int[101];
+        for (int sock = 0; sock < totalOfSocks; sock++)
+        {
+            countSockByColor[pileOfSocks[sock]]++;
+        }
+
+        int numberOfPairs = 0;
+        foreach (int socksByColor in countSockByColor)
+        {
+            int pairs = (int)(socksByColor / 2);
+
+            if (pairs > 0)
+            {
+                numberOfPairs += pairs;
+            }
+        }
+        return numberOfPairs;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
